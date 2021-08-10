c7d8635aa06a38e9e2ca7b4654eb602dfda16bbd
Daniel <esc.daniel91@gmail.com>
2021-08-10 16:41:23 -0400

    added templates

--- application.py
+++ application.py
@@ -11,5 +11,13 @@ def index():
 def about():
     return render_template("about.html", title = "About")
 
+@app.route("/register")
+def register():
+    return render_template("register.html", title= "Register")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title = "Login")
+
 if __name__ == '__main__':
     app.run(debug=True)

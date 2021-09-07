eaa4ba4bd4305e4e908e5de94601dd91397d2f87
Daniel <esc.daniel91@gmail.com>
2021-09-06 20:47:27 -0400

    changed account ui

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -52,6 +53,7 @@ class UpdateAccountInfo(FlaskForm):
                             validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    prof_pic = FileField('Update Profile Picture', validators=[FileAllowed(['png', 'jpg'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
